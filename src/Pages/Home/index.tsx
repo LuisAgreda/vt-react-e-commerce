44ef411f6ea10de5cf601b810c5b055a3e04e9aa
Luis Agreda <10234971+LuisAgreda@users.noreply.github.com>
2024-08-27 19:46:06 -0500

    feat: render cards in home page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,9 +1,25 @@
+import { useFetchData } from '../../hooks/useFetchData'
+
 import { Layout } from '../../Components/Layout'
+import { Card } from '../../Components/Card'
+
+import type { ProductsResponse } from '../../types'
 
 function Home() {
+  const { items } = useFetchData()
+
   return (
     <Layout>
       Home
+      <div className='w-full px-4 gap-4 grid justify-items-center grid-cols-[repeat(auto-fit,_minmax(224px,_1fr))]'>
+        {
+          (items as ProductsResponse[])?.map(item => (
+            <Card
+              key={ item.id }
+              data={ item } />
+          ))
+        }
+      </div>
     </Layout>
   )
 }
