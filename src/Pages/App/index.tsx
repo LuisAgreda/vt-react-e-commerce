27c2e37d58411159247b6134afde31a5201434b3
Luis Agreda <10234971+LuisAgreda@users.noreply.github.com>
2024-08-28 15:17:08 -0500

    feat: implementd IU component in app file

--- src/Pages/App/index.tsx
+++ src/Pages/App/index.tsx
@@ -1,32 +1,12 @@
-import { useRoutes, BrowserRouter } from 'react-router-dom'
+import { ShoppingProvider } from '../../Context'
+import { AppUI } from './AppUI'
 
-import { Navbar } from '../../Components/Navbar'
-import { Home } from '../Home'
-import { MyAccount } from '../MyAccount'
-import { MyOrder } from '../MyOrder'
-import { MyOrders } from '../MyOrders'
-import { NotFound } from '../NotFound'
-import { SignIn } from '../SignIn'
-
-const AppRoutes = () => {
-  const routes = useRoutes([
-    { path: '/', element: <Home /> },
-    { path: '/my-account', element: <MyAccount /> },
-    { path: '/my-order', element: <MyOrder /> },
-    { path: '/my-orders', element: <MyOrders /> },
-    { path: '/sign-in', element: <SignIn /> },
-    { path: '/*', element: <NotFound /> },
-  ])
-
-  return routes
-}
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <AppRoutes />
-    </BrowserRouter>
+    <ShoppingProvider>
+      <AppUI />
+    </ShoppingProvider>
   )
 }
 
