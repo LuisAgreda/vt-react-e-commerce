83f2c97ea1e9225d19fa4342dbb9011045a4bb99
Luis Agreda <10234971+LuisAgreda@users.noreply.github.com>
2024-09-02 15:42:52 -0500

    feat: show the selected details product in the component

--- src/Components/ProducDetail/index.tsx
+++ src/Components/ProducDetail/index.tsx
@@ -1,18 +1,57 @@
+import { ShoppingContext } from '../../Context'
+import { useContext } from 'react'
+
 import { XMarkIcon } from '@heroicons/react/24/solid'
+import { ProductsResponse } from '../../types'
 
 const ProductDetail = () => {
-  return (
-    <aside className="w-[22.5rem] h-[calc(100%-3.75rem)] bg-white flex flex-col border border-black rounded-lg fixed right-0 bottom-0">
-      <div className="flex justify-between items-center p-6">
-        <h2 className="font-medium text-xl">
-          detail
-        </h2>
-        <div>
-          <XMarkIcon className='w-6 h-6 text-black' />
+  const { isProductDetailOpen, closeProductDetail, currentProductDetail } = useContext(ShoppingContext)
+
+  const {
+    id,
+    title,
+    price,
+    description
+  } = currentProductDetail as ProductsResponse
+
+  if (isProductDetailOpen) {
+    return (
+      <aside className="w-[22.5rem] h-[calc(100%-3.75rem)] bg-white flex flex-col border border-black rounded-lg fixed right-0 bottom-0 z-10">
+        <div className="flex justify-between items-center p-6">
+          <h2 className="font-medium text-xl">
+            Detail
+          </h2>
+
+          <button
+            type="button"
+            onClick={ closeProductDetail }>
+            <XMarkIcon className="w-6 h-6 text-black" />
+          </button>
         </div>
-      </div>
-    </aside>
-  )
+
+        <figure className="px-6">
+          <img
+            className="w-full h-full rounded-lg"
+            src={ `https://picsum.photos/id/${id}/800` }
+            alt={ title } />
+        </figure>
+
+        <p className="flex flex-col p-6">
+          <span className="mb-2 text-2xl font-medium">
+            ${ price }
+          </span>
+
+          <span className="text-base font-medium">
+            { title }
+          </span>
+
+          <span className="text-sm">
+            { description }
+          </span>
+        </p>
+      </aside>
+    )
+  }
 }
 
 export { ProductDetail }
