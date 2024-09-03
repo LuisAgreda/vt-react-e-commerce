80f9f6aa036140b1ed8e27e28a24e1d6f535271f
Luis Agreda <10234971+LuisAgreda@users.noreply.github.com>
2024-09-03 15:42:18 -0500

    feat: implemented OrderCard component in checkout side menu component

--- src/Components/CheckoutSideMenu/index.tsx
+++ src/Components/CheckoutSideMenu/index.tsx
@@ -1,16 +1,18 @@
+import { OrderCard } from '../OrderCard'
+import { XMarkIcon } from '@heroicons/react/24/solid'
+
 import { ShoppingContext } from '../../Context'
 import { useContext } from 'react'
 
-import { XMarkIcon } from '@heroicons/react/24/solid'
-// import { ProductsResponse } from '../../types'
+import { ProductsResponse } from '../../types'
 
 const CheckoutSideMenu = () => {
-  const { isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen } = useContext(ShoppingContext)
+  const { isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen, cartProducts } = useContext(ShoppingContext)
 
   if (isCheckoutSideMenuOpen) {
     return (
-      <aside className="w-[22.5rem] h-[calc(100%-3.75rem)] bg-white flex flex-col border border-black rounded-lg fixed right-0 bottom-0 z-10">
-        <div className="flex justify-between items-center p-6">
+      <aside className="w-[22.5rem] h-[calc(100%-3.75rem)] bg-white flex flex-col border border-black rounded-lg overflow-x-auto fixed right-0 bottom-0 z-10">
+        <div className="p-6 bg-white/85 backdrop-blur flex justify-between items-center sticky top-0">
           <h2 className="font-medium text-xl">
             Cart
           </h2>
@@ -20,6 +22,16 @@ const CheckoutSideMenu = () => {
             onClick={ () => setIsCheckoutSideMenuOpen(false) }>
             <XMarkIcon className="w-6 h-6 text-black" />
           </button>
+        </div>
+
+        <div className="px-6 grid gap-6">
+          {
+            cartProducts.map((product: ProductsResponse) => (
+              <OrderCard
+                key={ product.id }
+                product={ product } />
+            ))
+          }
         </div>
       </aside>
     )
