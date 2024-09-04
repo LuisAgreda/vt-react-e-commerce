75dbed05b5a38ca6475324a1def84067350397e8
Luis Agreda <10234971+LuisAgreda@users.noreply.github.com>
2024-09-04 16:18:17 -0500

    chore: improved some styles and changed key id for order cards

--- src/Components/CheckoutSideMenu/index.tsx
+++ src/Components/CheckoutSideMenu/index.tsx
@@ -11,8 +11,8 @@ const CheckoutSideMenu = () => {
 
   if (isCheckoutSideMenuOpen) {
     return (
-      <aside className="w-[22.5rem] h-[calc(100%-3.75rem)] bg-white flex flex-col border border-black rounded-lg overflow-x-auto fixed right-0 bottom-0 z-10">
-        <div className="p-6 bg-white/85 backdrop-blur flex justify-between items-center sticky top-0">
+      <aside className="w-[22.5rem] h-[calc(100%-3.75rem)] pb-6 bg-white flex flex-col border border-black rounded-lg overflow-y-auto fixed right-0 bottom-0 z-10">
+        <div className="p-6 bg-white/85 backdrop-blur flex justify-between items-center sticky top-0 z-10">
           <h2 className="font-medium text-xl">
             Cart
           </h2>
@@ -24,11 +24,11 @@ const CheckoutSideMenu = () => {
           </button>
         </div>
 
-        <div className="px-6 grid gap-6">
+        <div className="px-6 grid gap-4">
           {
             cartProducts.map((product: ProductsResponse) => (
               <OrderCard
-                key={ product.id }
+                key={ `${product.title}-${product.id}` }
                 product={ product } />
             ))
           }
