9de2531a088821814e45acf1adc6f4cba816756f
Luis Agreda <10234971+LuisAgreda@users.noreply.github.com>
2024-09-04 17:06:10 -0500

    feat: implemented funtion totalPrice in checkoutSideMenu component

--- src/Components/CheckoutSideMenu/index.tsx
+++ src/Components/CheckoutSideMenu/index.tsx
@@ -1,17 +1,21 @@
+import { useContext } from 'react'
+import { totalPrice } from '../../utils'
+
 import { OrderCard } from '../OrderCard'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 
 import { ShoppingContext } from '../../Context'
-import { useContext } from 'react'
 
 import { ProductsResponse } from '../../types'
 
 const CheckoutSideMenu = () => {
   const { isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen, cartProducts } = useContext(ShoppingContext)
 
+  const productPrices = cartProducts.map(product => product.totalPrice!)
+
   if (isCheckoutSideMenuOpen) {
     return (
-      <aside className="w-[22.5rem] h-[calc(100%-3.75rem)] pb-6 bg-white flex flex-col border border-black rounded-lg overflow-y-auto fixed right-0 bottom-0 z-10">
+      <aside className="w-[22.5rem] h-[calc(100%-3.75rem)] bg-white flex flex-col border border-black rounded-lg overflow-y-auto fixed right-0 bottom-0 z-10">
         <div className="p-6 bg-white/85 backdrop-blur flex justify-between items-center sticky top-0 z-10">
           <h2 className="font-medium text-xl">
             Cart
@@ -32,6 +36,18 @@ const CheckoutSideMenu = () => {
                 product={ product } />
             ))
           }
+        </div>
+
+        <div className="p-6 bg-white sticky bottom-0">
+          <p className="flex justify-between items-center">
+            <span>
+              Total:
+            </span>
+
+            <span className="font-medium text-2xl">
+              $ { totalPrice(productPrices) }
+            </span>
+          </p>
         </div>
       </aside>
     )
