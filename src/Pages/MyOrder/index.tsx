0b67047c269fa697c193892e6c7dccf9bc714c8a
Luis Agreda <10234971+LuisAgreda@users.noreply.github.com>
2024-09-09 14:46:20 -0500

    feat: added button in my-order to go to my-orders page

--- src/Pages/MyOrder/index.tsx
+++ src/Pages/MyOrder/index.tsx
@@ -1,7 +1,9 @@
 import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 
 import { Layout } from '../../Components/Layout'
 import { OrderCard } from '../../Components/OrderCard'
+import { ChevronLeftIcon } from '@heroicons/react/24/solid'
 
 import { ShoppingContext } from '../../Context'
 
@@ -10,6 +12,18 @@ function MyOrder() {
 
   return (
     <Layout>
+      <div className="w-80 flex items-center justify-center relative">
+        <Link
+          to="/my-orders"
+          className="absolute left-0">
+            <ChevronLeftIcon className="w-6 h-6 text-black" />
+        </Link>
+
+        <span>
+          My Order
+        </span>
+      </div>
+
       <div className="flex flex-col flex-1 gap-4">
         {
           myOrders?.slice(-1)[0].products.map(product => (
