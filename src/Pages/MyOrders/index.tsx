9d9d13c7458ea950435ca0aa648ac68055d2bf11
Luis Agreda <10234971+LuisAgreda@users.noreply.github.com>
2024-09-09 14:45:31 -0500

    feat: show order summary info in my-orders page

--- src/Pages/MyOrders/index.tsx
+++ src/Pages/MyOrders/index.tsx
@@ -1,9 +1,27 @@
-import { Layout } from '../../Components/Layout'
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 
-function MyOrders() {
+import { ShoppingContext } from '../../Context'
+
+import { Layout } from '../../Components/Layout'
+import { OrdersCard } from '../../Components/OrdersCard'
+
+const MyOrders = () => {
+  const { myOrders } = useContext(ShoppingContext)
+
   return (
     <Layout>
-      MyOrders
+      My Orders
+
+      {
+        myOrders.map((order, index) => (
+          <Link
+            key={ index }
+            to={`/my-orders/${index}`}>
+            <OrdersCard order={ order } />
+          </Link>
+        ))
+      }
     </Layout>
   )
 }
