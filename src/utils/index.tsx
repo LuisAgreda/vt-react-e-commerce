d609564e3369ad00b5b073a9a772b2e8c7380e1b
Luis Agreda <10234971+LuisAgreda@users.noreply.github.com>
2024-09-09 14:41:08 -0500

    feat: added function to date format

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,1 +1,16 @@
 export const totalPrice = (values: number[]) => values.reduce((acc, value) => acc + value, 0)
+
+export const getDate = () => {
+  const date = new Date()
+
+  const day = date.getDate()
+  const month = date.getMonth() + 1
+  const year = date.getFullYear()
+
+  const hour = date.getHours()
+  const minutes = date.getMinutes()
+
+  const fullDate = `${day}/${month}/${year} ${hour}:${minutes}`
+
+  return fullDate
+}
